4de8059a82488dcbba9b61f23bcb407308f0badd
Rishab Parmar <parmar.rishab@gmail.com>
2020-08-13 17:23:39 +0530

    Added randomization to algorithm, UI updates and exit game functionality

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
     int max = 1000;
-    int min = 1;
+    int min = 1;   
     [SerializeField] TextMeshProUGUI guessText;
 
     // Start is called before the first frame update
     void Start()
     {        
-        guessText.text = ((max + min) / 2).ToString();
-        // max += 1: So that 1000 is inclusive of the range 1 - 1000
-        max += 1;        
+        guessText.text = (Random.Range(min, max)).ToString();              
     }
 
     public void GoHigher()
     {
-        min = (max + min) / 2;
-        guessText.text = (((max + min) / 2) - 1).ToString();
+        // + 1 because the Random won't guess that random number again as min and max are inclusive
+        min = int.Parse(guessText.text) + 1;
+        guessText.text = (Random.Range(min, max)).ToString();
     }
 
     public void GoLower()
     {
-        max = (max + min) / 2;
-        guessText.text = (((max + min) / 2) - 1).ToString();
+        // - 1 because the Random won't guess that random number again as min and max are inclusive
+        max = int.Parse(guessText.text) - 1;
+        guessText.text = (Random.Range(min, max)).ToString();
     }
 }
